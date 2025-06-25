826130194ff685176cd3eff99bf4c1957be8968f
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-06-25 08:56:15 +0700

    Implement timer

--- src/hardware_controller/hardware_controller/motors_controller.py
+++ src/hardware_controller/hardware_controller/motors_controller.py
@@ -3,54 +3,68 @@ import serial
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 
+
 class CommandNode(Node):
     def __init__(self):
-        super().__init__('motors_controller')
-
+        super().__init__("motors_controller")
 
         # Initialize serial communication with Arduino
         try:
-            self.ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
+            self.ser = serial.Serial("/dev/ttyUSB0", 115200, timeout=1)
             self.get_logger().info("Serial connection established with Arduino.")
         except serial.SerialException as e:
             self.get_logger().error(f"Failed to connect to Arduino: {e}")
             self.ser = None
             raise
-        
-        # Create a subscriber to the 'distance' topic
-        self.subscription = self.create_subscription(Twist, 'cmd_vel', self.listener_callback, 10)
-        
 
-    def listener_callback(self, msg: Twist) -> None:
-        if msg.linear.x != 0:
-            if msg.linear.x < 0:
-                command = 'BACKWARD\n'
+        self.last_ctl_command: Twist | None = None
+        self.timer = self.create_timer(0.05, self.motor_control)
+
+        # Create a subscriber to the 'distance' topic
+        self.subscription = self.create_subscription(
+            Twist,
+            "cmd_vel",
+            self.listener_callback,
+            10,
+        )
+
+    def motor_control(self) -> None:
+        if self.last_ctl_command is None:
+            return
+
+        if self.last_ctl_command.linear is not None and self.last_ctl_command.linear.x != 0:
+            if self.last_ctl_command.linear.x < 0:
+                command = "BACKWARD\n"
                 self.get_logger().info("Obstacle detected, moving backward.")
-            # elif msg.linear.x == 0:
-            #     if msg.angular.z == 0:
+            # elif self.last_ctl_command.linear.x == 0:
+            #     if self.last_ctl_command.angular.z == 0:
             #         command = 'STOP\n'
             #         self.get_logger().info("No signal from sensor, stopping.")
             else:
-                command = 'FORWARD\n'
+                command = "FORWARD\n"
                 self.get_logger().info("No obstacle detected, moving forward.")
-        elif msg.angular.z != 0:
-            if msg.angular.z > 0:
-                command = 'LEFT\n'
+        elif self.last_ctl_command.angular is not None and self.last_ctl_command.angular.z != 0:
+            if self.last_ctl_command.angular.z > 0:
+                command = "LEFT\n"
                 self.get_logger().info("Turning left")
-            # elif msg.linear.x == 0:
-            #     if msg.angular.z == 0:
+            # elif self.last_ctl_command.linear.x == 0:
+            #     if self.last_ctl_command.angular.z == 0:
             #         command = 'STOP\n'
             #         self.get_logger().info("No signal from sensor, stopping.")
             else:
-                command = 'RIGHT\n'
+                command = "RIGHT\n"
                 self.get_logger().info("Turning right")
         else:
-            command = 'STOP\n'
+            command = "STOP\n"
             self.get_logger().info("Stopping...")
-        
+
         if self.ser:
             self.ser.write(command.encode())
             self.get_logger().info(f"Sent command: {command.strip()}")
+
+    def listener_callback(self, msg: Twist) -> None:
+        self.last_ctl_command = msg
+
 
 def main(args=None):
     rclpy.init(args=args)
