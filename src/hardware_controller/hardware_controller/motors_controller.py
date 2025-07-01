ef645c1acfa0bc093488abb2cc4a5cf69368ce0e
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-07-01 11:55:09 +0700

    Change turning speed

--- src/hardware_controller/hardware_controller/motors_controller.py
+++ src/hardware_controller/hardware_controller/motors_controller.py
@@ -144,8 +144,8 @@ class MotorsControllerNode(Node):
         elif cmd.angular is not None and cmd.angular.z != 0:
             if cmd.angular.z > 0:
                 # command = "LEFT\n"
-                self.motor1.forward(13)
-                self.motor2.backward(20)
+                self.motor1.backward(18)
+                self.motor2.forward(25)
                 self.get_logger().info("Turning left")
             # elif cmd.linear.x == 0:
             #     if cmd.angular.z == 0:
@@ -153,8 +153,8 @@ class MotorsControllerNode(Node):
             #         self.get_logger().info("No signal from sensor, stopping.")
             else:
                 # command = "RIGHT\n"
-                self.motor1.backward(20)
-                self.motor2.forward(13)
+                self.motor1.forward(25)
+                self.motor2.backward(18)
                 self.get_logger().info("Turning right")
         else:
             # command = "STOP\n"
